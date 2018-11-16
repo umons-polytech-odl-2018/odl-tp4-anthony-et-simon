4b806f1ec3361fb5e3c937f63e58904cc26ef214
161240 <simon.renuart@student.umons.ac.be>
2018-11-16 14:45:30 +0100

    TP4

--- src/main/java/exercise1/ExceptionCatcher.java
+++ src/main/java/exercise1/ExceptionCatcher.java
@@ -1,13 +1,17 @@
 package exercise1;
 
 public class ExceptionCatcher {
-	private final ExceptionThrower exceptionThrower;
+	private final ExceptionThrower exceptionThrower;		//instancie ds test unitaire
 
-	public ExceptionCatcher(ExceptionThrower exceptionThrower) {
+	public ExceptionCatcher(ExceptionThrower exceptionThrower) {	//appelle fonction dans test unitaire
 		this.exceptionThrower = exceptionThrower;
 	}
 
 	void executeExceptionThrowerSafely(int value) {
-		exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		try {
+			exceptionThrower.throwACustomExceptionWhenValueIs42(value);			//analyse s'il y a exception
+		}
+		catch (RuntimeException e) {						//si oui, ça catch l'exception
+		}
 	}
 }
